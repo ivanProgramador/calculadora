2cba6b5eb9a9c3ded02474c85d71125ef4c83023
ivan <ivan-guita@hotmail.com>
2024-01-09 08:05:23 -0300

    bibliotecas de teste e interface gráfica adicionadas

--- teoria/conclusoes.js
+++ teoria/conclusoes.js
@@ -1,4 +1,12 @@
 /*
+ Versão usada nesse curso
+ npm install react-bootstrap@v1.0.0-beta.14 --save
+
+bibliotecas de teste 
+
+ npm install @testing-library/react @testing-library/jest-dom  --save-dev 
+
+
 
 
 */
